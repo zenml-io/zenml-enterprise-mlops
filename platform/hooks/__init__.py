59aec1e0f662e8195fe07deb2ea79c10e9bba428
Hamza Tahir <hamza@zenml.io>
2026-01-24 14:21:23 +0100

    Add Python package structure with __init__ files
    - Create platform package for governance (hooks, steps)
    - Create src package for ML code (pipelines, steps, utils)
    - Add proper Apache 2.0 license headers
    - Set up imports in __init__ files following ZenML best practices

---
+++ platform/hooks/__init__.py
@@ -0,0 +1,31 @@
+# Apache Software License 2.0
+#
+# Copyright (c) ZenML GmbH 2026. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Platform hooks for governance enforcement.
+
+Hooks are automatically executed at pipeline lifecycle events
+to enforce platform policies without modifying user code.
+"""
+
+from platform.hooks.mlflow_hook import mlflow_success_hook
+from platform.hooks.monitoring_hook import monitoring_success_hook
+from platform.hooks.compliance_hook import compliance_failure_hook
+
+__all__ = [
+    "mlflow_success_hook",
+    "monitoring_success_hook",
+    "compliance_failure_hook",
+]
---
+++ platform/steps/__init__.py
@@ -0,0 +1,29 @@
+# Apache Software License 2.0
+#
+# Copyright (c) ZenML GmbH 2026. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Platform-managed shared steps.
+
+These steps enforce governance policies and are required
+by the platform team to be included in all pipelines.
+"""
+
+from platform.steps.data_validation import validate_data_quality
+from platform.steps.model_validation import validate_model_performance
+
+__all__ = [
+    "validate_data_quality",
+    "validate_model_performance",
+]
